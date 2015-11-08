d8b233e0cf617b5016cc470603721d86e6f2bf5a
blaze97531 <core@vcore.(none)>
2015-11-08 11:47:41 -0800

    Modified the Markov Chain implementation so that each frame is only read one time, using a method like the hashing in Robin-Karp fingerprint substring search.

--- simplemarkov.py
+++ simplemarkov.py
@@ -26,7 +26,7 @@ print data
 
 #data = [[1, 0], [2, 0], [3, 0], [2, 0], [1, 0]]
 
-chain_depth = 1
+chain_depth = 50
 bin_size = .05
 def start_val(i):   # An arbitrary unique value representing i spaces before the start of the audio that doesn't interfere with the audio values
                     # Since these are generally in the range (-1, 1)
@@ -36,28 +36,29 @@ chain = {}  # The actual Markov chain
 uniq = []   # How many unique frequencies are there? This is to figure out whether we need to bucket similar frequencies in order to make a good Markov chain
 tc = chain
 
+num_buckets = 2 / bin_size + 1 # The total number of buckets, including one special bucket for before the start of the audio.
+bucket_index = 0 # The bucket index can be viewed as a base-num_buckets number with chain depth digits. The nth rightmost digit (counting the ones place as the first) is the bucket selected for the frequency n places back in the audio.
+for i in range(chain_depth): # Sets all digits of the bucket index equal to the special value
+	bucket_index = bucket_index * num_buckets + num_buckets - 1
+
 i = 0
 total_endpoints = 0
 num_categories = 0
-for i in range(len(data)):   # This is a pretty inefficient way to do things; you end up reading each value of data chain_depth times
+for i in range(len(data)):   
     if i % (5 * f.samplerate) == 0:   # Print progress every 5 seconds
         print i / f.samplerate
 
-    a = data[i][0]
-
     tc = chain
-    for j in range(chain_depth, 0, -1):
-        if i - j < 0:
-            val = start_val(j-i)
-        else:
-            val = data[i - j][0] - data[i - j][0] % bin_size    # Currently only looking at once channel of the audio
-                                                                # Not binning actual final frequencies, only groups within the chain
+    if bucket_index not in tc:
+        tc[bucket_index] = {}
 
-        if val not in tc:
-            tc[val] = {}
-        
-        tc = tc[val]
-        
+    tc = tc[bucket_index]
+
+    # Compute the bucket_index for the next iteration
+    a = data[i][0] 
+    val = (a - a % bin_size) / bin_size + 1 / bin_size   
+    bucket_index = (bucket_index * num_buckets + val) % (num_buckets ** chain_depth) # Shifts all digits of the bucket_index to the left, cuts off the highest digit, and adds a new rightmost digit.
+
     if data[i][0] not in tc:
         tc[data[i][0]] = 1
         total_endpoints += 1
@@ -76,20 +77,18 @@ print "Average endpoints: " + str(total_endpoints / num_categories)
 print "TE: " + str(total_endpoints) + " NC: " + str(num_categories)
 print str(len(uniq)) + " unique out of " + str(len(data))
 
+# Reset the bucket index to its starting value
+bucket_index = 0
+for i in range(chain_depth):
+	bucket_index = bucket_index * num_buckets + num_buckets - 1
+
 new_data = np.empty([f.samplerate * 15])
 for i in range(new_data.size):
     if i % (5 * f.samplerate) == 0:   # Print progress every 5 seconds
         print i / f.samplerate
-    tc = chain
+    tc = chain[bucket_index]
     new_datum = 0
-    for j in range(chain_depth, 0, -1):
-        if i - j < 0:
-            val = start_val(j-i)
-        else:
-            val = data[i - j][0] - data[i - j][0] % bin_size    # Currently only looking at once channel of the audio
-        
-        tc = tc[val]
-    
+
     #print tc
     weighted_index = random.randrange(tc["sum"])
     for k in tc.keys():
@@ -102,10 +101,13 @@ for i in range(new_data.size):
     new_data[i] = new_datum
     #print new_data[0:i]
 
+    # Compute the bucket_index for the next iteration
+    val = (new_data[i] - new_data[i] % bin_size) / bin_size + 1 / bin_size
+    bucket_index = (bucket_index * num_buckets + val) % (num_buckets ** chain_depth)
+
+
 f3 = audiolab.Sndfile('gen.wav', 'w', format, 1, f.samplerate)
 f3.write_frames(new_data)
 f3.close()
-            
-    
 
 #audiolab.play(data)
